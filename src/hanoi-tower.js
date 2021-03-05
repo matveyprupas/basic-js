7cb3bda48114de26d7ab22b762b93e535405a6eb
matveyprupas <matveyprupas@gmail.com>
2021-03-05 14:26:10 +0200

    feat: done next task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
+module.exports = function calculateHanoi(disk, speed) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let res = {};
+  res.turns = 2**disk - 1;
+  res.seconds = Math.floor(res.turns / (speed / 3600));
+  return res;
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,21 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+// module.exports = 
+function transform(arr) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
-};
+  
+  // if(Array.isArray(arr) == false) {
+  //   throw new Error();
+  // }
+
+
+}
+
+
+ console.log(transform(3));
+ console.log(transform(false));
+ console.log(transform(null));
+ console.log(transform(undefined));
+ console.log(transform({'foo': 'bar'}));
+
