9f601d2fd964b733c160f603961610f58850e4be
matveyprupas <matveyprupas@gmail.com>
2021-03-06 07:02:26 +0200

    feat: add transform-array. wrong tests

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,17 +5,60 @@ function transform(arr) {
   // throw new CustomError('Not implemented');
   // remove line with error and write your code here
   
-  // if(Array.isArray(arr) == false) {
-  //   throw new Error();
-  // }
+  if(Array.isArray(arr) == false) {
+    throw new Error();
+  }
+
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--double-next':
+        if (typeof(arr[i + 1]) != 'number') break;
+        result.push(arr[i + 1]);
+        break;
+
+      case '--double-prev':
+        if (typeof(arr[i - 1]) != 'number') break;
+        result.push(arr[i - 1]);
+        break;
+
+
+      case '--discard-prev':
+        // if (typeof(arr[i]) != 'number') break;
+        result.pop();
+        break;
+        
+      case '--discard-next':
+        if (typeof(arr[i]) != 'number') break;
+        i++;
+        break;
+  
+      default:
+        if (typeof(arr[i]) != 'number') break;
+        result.push(arr[i]);
+    }
+  }
+  return result;
 
 
 }
 
+console.log(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5])); //[1, 2, 3, 4, 5]
 
- console.log(transform(3));
- console.log(transform(false));
- console.log(transform(null));
- console.log(transform(undefined));
- console.log(transform({'foo': 'bar'}));
 
+//  console.log(transform([1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5])); //[1, 2, 3, 4, 5]
+//  console.log(transform([1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5])); //[1, 2, 3, 1337, 4, 5]
+//  console.log(transform(undefined));
+//  console.log(transform({'foo': 'bar'}));
+// const cases = [
+//   ['--discard-prev', 1, 2, 3],
+//   ['--double-prev', 1, 2, 3],
+//   [1, 2, 3, '--double-next'],
+//   [1, 2, 3, '--discard-next']
+// ];
+// //  console.log(transform());
+
+// cases.forEach(currCase => {
+//   console.log(transform(currCase));
+// });
+// [1, 2, 3]
