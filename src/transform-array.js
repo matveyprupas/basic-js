79576e7b0943114e9e57ab1971fd9e9d6cfffeec
matveyprupas <matveyprupas@gmail.com>
2021-03-09 15:49:01 +0200

    feat: done transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,64 +1,28 @@
-// const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error");
 
-// module.exports = 
-function transform(arr) {
-  // throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-  
+module.exports = function transform(arr) {
   if(Array.isArray(arr) == false) {
     throw new Error();
   }
-
   let result = [];
   for (let i = 0; i < arr.length; i++) {
-    switch (arr[i]) {
-      case '--double-next':
-        if (typeof(arr[i + 1]) != 'number') break;
-        result.push(arr[i + 1]);
-        break;
-
-      case '--double-prev':
-        if (typeof(arr[i - 1]) != 'number') break;
-        result.push(arr[i - 1]);
-        break;
-
-
-      case '--discard-prev':
-        // if (typeof(arr[i]) != 'number') break;
-        result.pop();
-        break;
-        
+    switch(arr[i]) {
       case '--discard-next':
-        if (typeof(arr[i]) != 'number') break;
         i++;
         break;
-  
+      case '--discard-prev':
+        if(result.length > 0 && arr[i - 2] != '--discard-next') result.pop();
+        break;
+      case '--double-next':
+        if(i < arr.length - 1) result.push(arr[i + 1]);
+        break;
+      case '--double-prev':
+        if(result.length > 0 && arr[i - 2] != '--discard-next') result.push(arr[i - 1]);
+        break;
       default:
-        if (typeof(arr[i]) != 'number') break;
         result.push(arr[i]);
+        break;
     }
   }
   return result;
-
-
 }
-
-console.log(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5])); //[1, 2, 3, 4, 5]
-
-
-//  console.log(transform([1, 2, 3, '--discard-next', 1337, '--discard-prev', 4, 5])); //[1, 2, 3, 4, 5]
-//  console.log(transform([1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5])); //[1, 2, 3, 1337, 4, 5]
-//  console.log(transform(undefined));
-//  console.log(transform({'foo': 'bar'}));
-// const cases = [
-//   ['--discard-prev', 1, 2, 3],
-//   ['--double-prev', 1, 2, 3],
-//   [1, 2, 3, '--double-next'],
-//   [1, 2, 3, '--discard-next']
-// ];
-// //  console.log(transform());
-
-// cases.forEach(currCase => {
-//   console.log(transform(currCase));
-// });
-// [1, 2, 3]
